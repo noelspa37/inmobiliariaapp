292fa39bf3cb9985695e9f0660824b6ac69dfec4
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-09-01 23:42:09 -0500

    feat: BarSession component added

--- src/componentes/layout/bar/BarSession.js
+++ src/componentes/layout/bar/BarSession.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
-import { Toolbar, Typography } from '@material-ui/core'
+import { Toolbar, Typography, withStyles, Button } from '@material-ui/core'
+
+const styles = theme => ({
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex'
+        }
+    }
+})
 
 class BarSession extends Component {
     render() {
+        const { classes } = this.props
+
         return (
             <div>
                 <Toolbar>
                     <Typography variant='h6'>
                         VAXI HOMES
                     </Typography>
+                    <div className={classes.sectionDesktop}>
+                        <Button>Login</Button>
+                    </div>
                 </Toolbar>
             </div>
         )
     }
 }
 
-export default BarSession
+export default withStyles(styles)(BarSession)
