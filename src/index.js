44912c406bd474578e0be213fe43b83f10025382
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-09-08 22:37:17 -0500

    chore: install recompose

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './server/firebase'
+import Firebase, { FirebaseContext } from './server'
 
 //const FirebaseContext = React.createContext()
 
